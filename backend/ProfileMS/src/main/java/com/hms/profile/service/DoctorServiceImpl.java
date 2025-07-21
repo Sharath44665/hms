95a8fa1b07544ac1fff252067000d8a93d6c0295
sharath44665 <sharathwelcomesyouin@gmail.com>
2025-07-21 17:59:11 +0530

    ep 8 - backend design for doctor, patient

---
+++ backend/ProfileMS/src/main/java/com/hms/profile/service/DoctorServiceImpl.java
@@ -0,0 +1,29 @@
+package com.hms.profile.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hms.profile.dto.DoctorDTO;
+import com.hms.profile.exceptioin.HmsException;
+import com.hms.profile.repository.DoctorRepository;
+
+@Service
+public class DoctorServiceImpl implements DoctorService {
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @Override
+    public Long addDoctor(DoctorDTO doctorDTO) throws HmsException {
+        if (doctorDTO.getEmail() != null && doctorRepository.findByEmail(doctorDTO.getEmail()).isPresent())
+            throw new HmsException("DOCTOR_ALREADY_EXIST");
+        if (doctorDTO.getLicenseNo() != null && doctorRepository.findByLicenseNo(doctorDTO.getLicenseNo()).isPresent())
+            throw new HmsException("DOCTOR_ALREADY_EXIST");
+        return doctorRepository.save(doctorDTO.toEntity()).getId();
+    }
+
+    @Override
+    public DoctorDTO getDoctorById(Long id) throws HmsException {
+        return doctorRepository.findById(id).orElseThrow(() -> new HmsException("DOCTOR_NOT_FOUND")).toDTO();
+    }
+
+}
